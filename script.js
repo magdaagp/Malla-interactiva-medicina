68e7fce64ce6296ff4cdad0f8abccc6e43d6d0a7
magdaagp <m.guajardo.2@ug.uchile.cl>
2025-07-20 23:20:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,130 @@
 const ramos = [
-  { nombre: "Matemáticas", creditos: 3, requisitos: [], semestre: 1 },
-  { nombre: "Química", creditos: 6, requisitos: [], semestre: 1 },
-  { nombre: "Anatomía I", creditos: 6, requisitos: [], semestre: 1 },
-  { nombre: "Introducción a la profesión médica", creditos: 6, requisitos: [], semestre: 1 },
-  { nombre: "Medicina y sociedad", creditos: 4, requisitos: [], semestre: 1 },
-  { nombre: "Formación general I", creditos: 2, requisitos: [], semestre: 1 },
-  { nombre: "Inglés I", creditos: 3, requisitos: [], semestre: 1 },
-  { nombre: "Física", creditos: 5, requisitos: [], semestre: 2 },
-  { nombre: "Biología celular y molecular", creditos: 6, requisitos: [], semestre: 2 },
-  { nombre: "Anatomía II", creditos: 4, requisitos: ["Anatomía I"], semestre: 2 },
-  { nombre: "Histología y embriología", creditos: 6, requisitos: [], semestre: 2 },
-  { nombre: "Medicina, persona y sociedad", creditos: 4, requisitos: ["Medicina y sociedad"], semestre: 2 },
-  { nombre: "Introducción a la salud pública", creditos: 2, requisitos: ["Introducción a la profesión médica"], semestre: 2 },
-  { nombre: "Inglés II", creditos: 3, requisitos: [], semestre: 2 }
+  {
+    "nombre": "Matemáticas",
+    "creditos": 3,
+    "requisitos": [],
+    "semestre": 1
+  },
+  {
+    "nombre": "Química",
+    "creditos": 6,
+    "requisitos": [],
+    "semestre": 1
+  },
+  {
+    "nombre": "Anatomía I",
+    "creditos": 6,
+    "requisitos": [],
+    "semestre": 1
+  },
+  {
+    "nombre": "Introducción a la profesión médica",
+    "creditos": 6,
+    "requisitos": [],
+    "semestre": 1
+  },
+  {
+    "nombre": "Medicina y sociedad",
+    "creditos": 4,
+    "requisitos": [],
+    "semestre": 1
+  },
+  {
+    "nombre": "Formación general I",
+    "creditos": 2,
+    "requisitos": [],
+    "semestre": 1
+  },
+  {
+    "nombre": "Inglés",
+    "creditos": 3,
+    "requisitos": [],
+    "semestre": 1
+  },
+  {
+    "nombre": "Física",
+    "creditos": 5,
+    "requisitos": [],
+    "semestre": 2
+  },
+  {
+    "nombre": "Biología celular y molecular",
+    "creditos": 6,
+    "requisitos": [],
+    "semestre": 2
+  },
+  {
+    "nombre": "Anatomía II",
+    "creditos": 4,
+    "requisitos": ["Anatomía I"],
+    "semestre": 2
+  },
+  {
+    "nombre": "Histología y embriología",
+    "creditos": 6,
+    "requisitos": [],
+    "semestre": 2
+  },
+  {
+    "nombre": "Medicina, persona y sociedad",
+    "creditos": 4,
+    "requisitos": ["Medicina y sociedad"],
+    "semestre": 2
+  },
+  {
+    "nombre": "Introducción a la salud pública",
+    "creditos": 2,
+    "requisitos": ["Introducción a la profesión médica"],
+    "semestre": 2
+  },
+  {
+    "nombre": "Inglés II",
+    "creditos": 3,
+    "requisitos": ["Inglés"],
+    "semestre": 2
+  },
+  {
+    "nombre": "Fisiología I",
+    "creditos": 8,
+    "requisitos": ["Anatomía II", "Biología celular y molecular", "Física"],
+    "semestre": 3
+  },
+  {
+    "nombre": "Bioquímica",
+    "creditos": 4,
+    "requisitos": ["Biología celular y molecular"],
+    "semestre": 3
+  },
+  {
+    "nombre": "Genética",
+    "creditos": 5,
+    "requisitos": ["Biología celular y molecular"],
+    "semestre": 3
+  },
+  {
+    "nombre": "Unidad de investigación I",
+    "creditos": 2,
+    "requisitos": [],
+    "semestre": 3
+  },
+  {
+    "nombre": "Semiología I",
+    "creditos": 6,
+    "requisitos": ["Introducción a la profesión médica", "Medicina, persona y sociedad"],
+    "semestre": 3
+  },
+  {
+    "nombre": "Bioética",
+    "creditos": 3,
+    "requisitos": [],
+    "semestre": 3
+  },
+  {
+    "nombre": "Bioestadística",
+    "creditos": 2,
+    "requisitos": ["Matemáticas"],
+    "semestre": 3
+  }
 ];
 
 const contenedor = document.getElementById("contenedor-malla");
