97e6b9b1c899069f5fde2f976164a00137d1c4f6
magdaagp <m.guajardo.2@ug.uchile.cl>
2025-07-20 21:40:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,49 @@
-function verDetalle(celda) {
-  const contenido = celda.textContent;
-  alert("Clase: " + contenido + "\nPuedes agregar más info aquí.");
+const ramos = [
+  {
+    nombre: "Anatomía I",
+    creditos: 6,
+    requisitos: "Ninguno"
+  },
+  {
+    nombre: "Fisiología I",
+    creditos: 8,
+    requisitos: "Anatomía II, Biología celular y molecular, Física"
+  },
+  {
+    nombre: "Bioquímica",
+    creditos: 4,
+    requisitos: "Biología celular y molecular"
+  },
+  {
+    nombre: "Ginecología - Obstetricia",
+    creditos: 10,
+    requisitos: "Cirugía"
+  },
+  {
+    nombre: "Psiquiatría infantil",
+    creditos: 2,
+    requisitos: "Medicina general familiar II"
+  }
+  // ← Aquí agregaremos todos los demás ramos después
+];
+
+const mallaDiv = document.querySelector(".malla");
+
+ramos.forEach(ramo => {
+  const div = document.createElement("div");
+  div.className = "ramo";
+  div.innerText = `${ramo.nombre} (${ramo.creditos} cr.)`;
+  div.onclick = () => mostrarModal(ramo);
+  mallaDiv.appendChild(div);
+});
+
+function mostrarModal(ramo) {
+  document.getElementById("modal-nombre").innerText = ramo.nombre;
+  document.getElementById("modal-creditos").innerText = ramo.creditos;
+  document.getElementById("modal-requisitos").innerText = ramo.requisitos;
+  document.getElementById("modal").style.display = "flex";
+}
+
+function cerrarModal() {
+  document.getElementById("modal").style.display = "none";
 }
